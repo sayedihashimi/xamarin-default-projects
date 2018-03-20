290d47fc9f3296d60f87a0b9627613e4da67121d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-03-20 14:38:52 -0400

    git init

---
+++ from-vsm/FormsApp01/FormsApp01/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FormsApp01
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
